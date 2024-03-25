14d78f88079949614950ec21ff30a152232dedd2
James Andrade Cevallos <james0jesus@gmail.com>
2024-03-25 16:15:56 +0100

    Creacion de Controlador Visitante,
    vistas para productos y entrenamiento

---
+++ Proyecto10AbrilFront/Controllers/VisitanteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto10AbrilFront.Controllers
+{
+    public class VisitanteController : Controller
+    {
+
+        //vistas para los visitantes: tienda , productos 
+
+        // GET: Visitante
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ProductosEquipaciones()
+        {
+            return View();
+        }
+
+        public ActionResult ProductosEntrenamiento()
+        {
+            return View();
+        }
+
+    }
+}
