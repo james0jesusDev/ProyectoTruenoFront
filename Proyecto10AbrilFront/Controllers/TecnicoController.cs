1e8c403bb029e7e8dcff9f1d42e550f126f65f73
James Andrade Cevallos <james0jesus@gmail.com>
2024-03-31 19:41:18 +0200

    Modificaciones Vistas Tecnico/Entrenador

---
+++ Proyecto10AbrilFront/Controllers/TecnicoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto10AbrilFront.Controllers
+{
+    public class TecnicoController : Controller
+    {
+        // GET: Tecnico
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult JugadoresTecnico()
+        {
+            return View();
+
+        }
+
+        public ActionResult CuerpoTecnico()
+        {
+            return View();
+
+        }
+
+
+
+    }
+}
